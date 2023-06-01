1807f4d4253c0f0f89cadf8f0ecf60af5ee67948
Narahari N Kambaluru <narahari.n@gmail.com>
2023-06-01 22:30:15 +0530

    Update TryinOutSymPy.py

--- TryinOutSymPy.py
+++ TryinOutSymPy.py
@@ -1,4 +1,11 @@
 import sympy as sym
+# Using sympy as a calculator
+
 a = sym.Rational(1, 2)
 print(a)
 print(a*2)
+print(sym.pi)
+print(sym.pi**2)
+print(sym.pi.evalf())
+print(sym.exp(1).evalf())
+print((sym.pi + sym.exp(1)).evalf())
