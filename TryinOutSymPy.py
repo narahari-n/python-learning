b15e074967d6e219f6ee640c0d3923949ed52183
Narahari N Kambaluru <narahari.n@gmail.com>
2023-06-02 11:17:59 +0530

    Update TryinOutSymPy.py

--- TryinOutSymPy.py
+++ TryinOutSymPy.py
@@ -1,10 +1,12 @@
 import sympy as sym
 import os #For clearing the terminal (or console) screen everytime by calling os.system('cls')
+from sympy import *
 
 a = sym.Rational(1, 2)
 os.system('cls')    #This is equivalent to clrscr() in c or clc in MATLAB.
 
-""" # Using sympy as a calculator
+"""
+# Using sympy as a calculator
 print(a)
 print(a*2)
 print(sym.pi)
@@ -15,15 +17,17 @@ print((sym.pi + sym.exp(1)).evalf())
 
 b = (sym.oo > 99999999) #sym.oo is infinity
 print(b)
-print(sym.oo+1)     #infinity+1 is also infinity """
+print(sym.oo+1)     #infinity+1 is also infinity
+"""
 
 """ Exercises
 
 Calculate \sqrt{2} with 100 decimals.
-Calculate 1/2 + 1/3 in rational arithmetic. """
+Calculate 1/2 + 1/3 in rational arithmetic.
 
-
+print(N(sqrt(2),100))   #obtaining 100 decimal places of any irrational number
 
 num1 = sym.Rational(1,2)
 num2 = sym.Rational(1,3)
 print(num1+num2)
+"""
