0a8e808c8b4c7f1eb60547a3e57a1412585aaa06
Narahari N Kambaluru <narahari.n@gmail.com>
2023-06-02 12:28:25 +0530

    Update TryinOutSymPy.py

--- TryinOutSymPy.py
+++ TryinOutSymPy.py
@@ -25,9 +25,32 @@ print(sym.oo+1)     #infinity+1 is also infinity
 Calculate \sqrt{2} with 100 decimals.
 Calculate 1/2 + 1/3 in rational arithmetic.
 
-print(N(sqrt(2),100))   #obtaining 100 decimal places of any irrational number
+print(N(sqrt(2),100))   #obtaining 100 decimal places of any irrational number (https://docs.sympy.org/latest/modules/evalf.html)
 
 num1 = sym.Rational(1,2)
 num2 = sym.Rational(1,3)
 print(num1+num2)
 """
+"""
+=============================================================
+"""
+
+# Symbols - Algebraic manipulations
+
+sym.init_printing(use_unicode=False, wrap_line=True)
+x = sym.Symbol('x')
+y = sym.Symbol('y')
+#z = x+y+x-y
+#z = sym.expand((x+y)**2)
+#z = sym.expand(x + y, complex=True)
+# print(sym.I * sym.im(x) + sym.I * sym.im(y) + sym.re(x) + sym.re(y))
+# print(sym.expand(sym.cos(x + y), trig=True))
+# print(sym.cos(x) * sym.cos(y) - sym.sin(x) * sym.sin(y))
+# print(sym.simplify((x**2 + x * y) / x))
+
+# Exercises:
+# 1. Calculate the expanded form of (x+y)^6
+# 2. Simplify the trigonometric expression sin(x)/cos(x)
+
+print(sym.expand((x+y)**6))
+print(sym.simplify((sin(x)/cos(x)),trig=True))
